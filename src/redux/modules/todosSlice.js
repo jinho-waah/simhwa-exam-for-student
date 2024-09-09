2227f1f52ebe689b3fbdf090af6a6915ec0068c2
jinho-waah <andrewjo950515@gmail.com>
2024-09-09 10:58:30 +0900

    update

--- src/redux/modules/todosSlice.js
+++ src/redux/modules/todosSlice.js
@@ -30,12 +30,8 @@ const todosSlice = createSlice({
   name: "todos",
   initialState,
   reducers: {
-    addTodo: (state, action) => {
-      return [...state, action.payload];
-    },
-    deleteTodo: (state, action) => {
-      return state.filter((todo) => todo.id !== action.payload);
-    },
+    addTodo: (state, action) => {},
+    deleteTodo: (state, action) => {},
   },
   extraReducers: {
     [__addToDo.fulfilled]: (state, action) => {
